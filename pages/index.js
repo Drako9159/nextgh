ebb986565e094780499c689d1d46819cd7dcc78d
drako9159 <drako9159@gmail.com>
2022-10-20 13:50:08 -0500

    change test

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Next Js CI/CD con Github actions
+          Next Js CI/CD con Github actions PM2
         </h1>
 
         <p className={styles.description}>
